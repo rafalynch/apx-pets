66c93e1062c11421e7dfafd49b3b08554d3758be
Rafa Lynch <rafa_lynch@hotmail.com>
2022-01-07 16:46:57 -0300

    Fix cors error

--- backend-src/index.ts
+++ backend-src/index.ts
@@ -1,5 +1,6 @@
-// import * as dotenv from "dotenv";
-// dotenv.config();
+//import * as dotenv from "dotenv";
+//dotenv.config();
+import * as cors from "cors";
 import * as express from "express";
 import * as bearerToken from "bearer-token";
 import * as path from "path";
@@ -26,13 +27,15 @@ import {
 import { sequelize } from "./models";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3003;
 
 app.use(
   express.json({
     limit: "50mb",
   })
 );
+
+app.use(cors());
 
 //sequelize.sync({ alter: true });
 
