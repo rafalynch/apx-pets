da0ca256d74873d3882d4c9396bf7c6d06086c9b
Rafa Lynch <rafa_lynch@hotmail.com>
2022-01-13 13:49:55 -0300

    Fix patch request

--- backend-src/index.ts
+++ backend-src/index.ts
@@ -1,5 +1,5 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+//import * as dotenv from "dotenv";
+//dotenv.config();
 import * as cors from "cors";
 import * as express from "express";
 import * as bearerToken from "bearer-token";
